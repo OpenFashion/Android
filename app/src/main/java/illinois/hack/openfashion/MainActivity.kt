a86706eb1216b5a77682d334fad111e1c0adf7fc
mlee108 <18340464+mlee108@users.noreply.github.com>
2018-02-24 06:42:59 -0600

    camera opens

--- app/src/main/java/illinois/hack/openfashion/MainActivity.kt
+++ app/src/main/java/illinois/hack/openfashion/MainActivity.kt
@@ -1,19 +1,93 @@
 package illinois.hack.openfashion
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.constraint.ConstraintLayout
+import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class MainActivity : AppCompatActivity() {
 
+    private val CAMERA_REQUEST_CODE: Int = 0
 
+    private lateinit var button: Button
+    private lateinit var layout: ConstraintLayout
+    private lateinit var mStorage: StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStorage = FirebaseStorage.getInstance().reference
+
+        layout = findViewById(R.id.layout)
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                    // supposed to show reasoning here but why should i
+                }
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+
+            } else {
+                goToCamera()
+            }
+        }
     }
 
-
-    override fun onStart() {
-        super.onStart()
+    fun goToCamera() {
+        var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+
+                    // permission was granted, yay!
+
+                    goToCamera()
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                    Snackbar.make(layout, "BOOOOOOO", Snackbar.LENGTH_LONG).show()
+                }
+                return
+            }
+
+            else -> {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            var uri: Uri = data.data
+
+            var filepath: StorageReference = mStorage.child("Photos").child(uri.lastPathSegment)
+            filepath.putFile(uri).addOnSuccessListener {
+                Snackbar.make(layout, "Uploading finished", Snackbar.LENGTH_LONG).show()
+            }
+
+        }
+    }
+
 }
