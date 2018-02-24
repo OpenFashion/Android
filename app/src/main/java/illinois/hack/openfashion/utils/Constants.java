f629fe5e6e4993740d883ad6293c6a344298513a
Tim Choh <tchoh2@uic.edu>
2018-02-24 12:10:34 -0600

    updated for firebase working

---
+++ app/src/main/java/illinois/hack/openfashion/utils/Constants.java
@@ -0,0 +1,13 @@
+package illinois.hack.openfashion.utils;
+
+/**
+ * Created by Tim on 2/24/2018.
+ */
+
+public class Constants {
+
+    public static final String FIREBASE_URL = "https://openfashion-196206.firebaseio.com/";
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
+    public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
+}
